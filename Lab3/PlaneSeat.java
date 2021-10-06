ef7ee2085aef86d8cac3ae344770122e3aee21f4
teamoteh <lim1473@gmail.com>
2021-10-07 01:34:08 +0800

    Added assidned to be false by default

--- Lab3/PlaneSeat.java
+++ Lab3/PlaneSeat.java
@@ -2,11 +2,12 @@ package Lab3;
 
 public class PlaneSeat{
     private int seatId;
-    private boolean assigned;
+    private boolean assigned = false;
     private int customerId;
     
     public PlaneSeat(int seat_id){
         this.seatId = seat_id;
+        this.customerId = -1;
     }
     public int getSeatID(){
         return seatId;
@@ -24,5 +25,7 @@ public class PlaneSeat{
     }
     void unAssign(){
         this.assigned = false;
+        this.customerId = -1;
+
     }
 }
